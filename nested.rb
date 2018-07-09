e4e7a1ceef806bb8f25b655a6cf879d82eec90ab
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-09 05:57:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumito] = {}
-programmer_hash[:yukihiro_matsumito][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumito][:languages] = ["LISP","C"]   
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]   
 end
 
 def changing_alan
